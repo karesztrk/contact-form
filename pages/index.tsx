d73d7a653caeef7fca7a2f2a9139273b6619db46
Károly Török <torok.karoly.krisztian@gmail.com>
2021-02-24 13:51:39 +0100

    Chakra UI

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+  <Layout title='Hello Contact Form'>
+    <h1>Hello Contact Form 👋</h1>
+    <p>Hello world</p>
+  </Layout>
+);
+
+export default IndexPage;
